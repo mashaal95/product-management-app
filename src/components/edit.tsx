2cd0a80bcf84c339047f6ac407fa8993c483f162
Mashaal Ahmed <pepsparks@gmail.com>
2022-10-29 04:39:33 +1100

    Completed adding products onto the form

--- src/components/edit.tsx
+++ src/components/edit.tsx
@@ -1,11 +1,18 @@
+import { IconButton } from '@mui/material'
 import Button from '@mui/material/Button'
 import React from 'react'
+import { Product } from '../views/product-table'
+import DeleteIcon from '@mui/icons-material/Delete';
 // import { Link } from 'react-router-dom'
 
 
-const EditBookButton = ({book} : any) => {
+const EditBookButton = ({} : Product) => {
     return (
-        <></>
+        <>
+          <IconButton>
+            <DeleteIcon />
+          </IconButton>
+        </>
         // <Link to={`/books/edit/${book.isbn}`}>
         //     <Button>
         //         Edit
--- src/components/form-modal.tsx
+++ src/components/form-modal.tsx
@@ -2,10 +2,14 @@ import * as React from "react";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import Dialog from "@mui/material/Dialog";
-import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
 import DialogTitle from "@mui/material/DialogTitle";
-import { FormControlLabel, Switch } from "@mui/material";
+import { FormControlLabel, Input, Select, Switch } from "@mui/material";
+import { useForm, SubmitHandler, Controller } from "react-hook-form";
+import { Stack } from "@mui/system";
+import productManagementService from "../services/product-management-service";
+import { Product } from "../views/product-table";
+import uuid from 'react-uuid';
 
 
 interface FormProps {
@@ -21,47 +25,97 @@ interface FormProps {
 
 const FormDialog = (props: FormProps) => {
 
+    const { control, handleSubmit } = useForm<FormProps>();
+
+  const onSubmit: SubmitHandler<FormProps> = data => {
+    const ProductObject: Product = {
+        id: uuid(),
+        name: data.name ?? "",
+        price: data.price ?? 0.0,
+        type: data.type ?? "",
+        active: data.active ?? ""
+    }
+    productManagementService
+        .createProduct(ProductObject)
+        .then(response => window.location.reload())
+  };
+
   return (
     <>
       <Dialog open={props.open} onClose={props.close}>
         <DialogTitle>{props.formTitle}</DialogTitle>
         <DialogContent>
-          <TextField
-            margin="dense"
-            id="name"
-            label="Name"
-            type="text"
-            fullWidth
-            variant="standard"
-            defaultValue= {props.name}
-          />
-          <TextField
-            margin="dense"
-            id="price"
-            label="Price"
-            type="number"
-            fullWidth
-            variant="standard"
-            defaultValue= {props.price}
-          />
-          <TextField
-            margin="dense"
-            id="type"
-            label="Type"
-            type="text"
-            fullWidth
-            variant="standard"
-            defaultValue= {props.type}
-          />
-          <FormControlLabel
-            control={<Switch />}
-            label="Active"
-          />
+
+        <form onSubmit={handleSubmit(onSubmit)}>
+      <Controller
+        name="name"
+        control={control}
+        defaultValue= {props.name}
+        render={({ field }) => <TextField
+        margin="dense"
+        id="name"
+        label="Name"
+        type="text"
+        fullWidth
+        variant="standard"
+        defaultValue= {props.name}
+
+        {...field}
+      />}
+      />
+       <Controller
+        name="price"
+        control={control}
+        defaultValue= {props.price}
+        render={({ field }) => <TextField
+        margin="dense"
+        id="name"
+        label="Price"
+        type="text"
+        fullWidth
+        variant="standard"
+        defaultValue= {props.price}
+
+        {...field}
+      />}
+      />
+       <Controller
+        name="type"
+        control={control}
+        defaultValue=""
+        render={({ field }) => <TextField
+        margin="dense"
+        id="name"
+        label="Type"
+        type="text"
+        fullWidth
+        variant="standard"
+        defaultValue= {props.type}
+
+        {...field}
+      />}
+      />
+
+<Controller
+        name="name"
+        control={control}
+        render={({ field }) =>  <FormControlLabel
+        control={<Switch />}
+        label="Active"
+      /> }
+
+      />
+   <div></div>
+   <div></div>
+   <div></div>
+      
+      <Stack  spacing={2} direction="row" justifyContent={"flex-end"}>
+        <Button style={{float: "right"}} variant="contained"  onClick={props.close}>Cancel</Button>
+         <Button style={{float: "right"}} variant="contained" color="success" type="submit" onClick={props.close}>Save</Button>
+         </Stack>
+    </form>
         </DialogContent>
-        <DialogActions>
-          <Button onClick={props.close}>Cancel</Button>
-          <Button onClick={props.close}>Save</Button>
-        </DialogActions>
+   
       </Dialog>
     </>
   );
